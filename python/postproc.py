e660fa68716ab869a649aa0912ac26ebfb658b1e
RWTHLHK <linghao.kongrwth@gmail.com>
2023-10-26 16:47:15 +0200

    change code structure

--- python/postproc.py
+++ python/postproc.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
 
     result_file = '/home/p0021070/damask/test/grid_load.hdf5'
     images = '/home/p0021070/damask/test/evaluation_images'
+    phases = ['Alpha', 'Beta']
 
     result = damask.Result(result_file)
     # result.add_strain(F='F')
@@ -40,57 +41,39 @@ if __name__ == "__main__":
         strain_vector.append(strain_mean)
 
     for _, stress in stress_eq.items():
-        stress_mean = np.abs(stress[:, 2, 2].mean()/1e6)
+        stress_mean = np.abs(stress[:, 2, 2].mean()) #Pa
         stress_vector.append(stress_mean)
 
+    # Partitioned Flowcurves
+    phase2stress = {}
+    phase2strain = {}
+    for phase in phases:
+        phase2stress[phase] = []
+        phase2strain[phase] = []
 
-    plt.plot(strain_vector, stress_vector, label='True Stress')
-    plt.legend()
+    for key, val in result.get('sigma').items():
+        for subkey, subvalue in val.items():
+            for phase in phases:
+                if phase in subkey:
+                    phase2stress[phase].append(np.mean(subvalue[:,2,2])) #Pa
+
+    for key, val in result.get('epsilon_V^0.0(F)').items():
+        for subkey, subvalue in val.items():
+            for phase in phases:
+                if phase in subkey:
+                    phase2strain[phase].append(np.mean(subvalue[:,2,2]))
+
+    #plot results
+    plt.plot(strain_vector, np.asarray(stress_vector)/1e6, label='True Stress') #MPa
     plt.xlabel("True Strain")
     plt.ylabel("True Stress (MPa)")
+
+    for phase in phases:
+        plt.plot(phase2strain[phase], np.asarray(phase2stress[phase])/1e6, label=f'Sim_{phase}')#MPa
+
+    plt.legend()
     plt.savefig(f"{images}/test_result.png")
 
-    # # CRSS Distribution
-    # crss_start= np.mean(result.view('increment', 2).get('xi_sl'))
-    # crss_96 = np.mean(result.view('increment', 100).get('xi_sl'))
-    # crss_146 = np.mean(result.view('increment', 154).get('xi_sl'))
-    # crss_216 = np.mean(result.view('increment', -1).get('xi_sl'))
-    # print(crss_start)
 
-    # # Partitioned Flowcurves
-    # ferrite_stress = list()
-    # martensite_stress = list()
-    # for key, val in result.get('sigma').items():
-    #     temp = list()
-    #     for subkey, subvalue in val.items():
-    #         if 'Ferrite' in subkey:
-    #             temp.append(np.mean(subvalue[:,0,0]))
-    #         else:
-    #             martensite_stress.append(np.mean(subvalue[:,0,0]))
-    #     ferrite_stress.append(np.mean(temp))
-
-    # ferrite_strain = list()
-    # martensite_strain = list()
-    # for key, val in result.get('epsilon_V^0.0(F)').items():
-    #     temp = list()
-    #     for subkey, subvalue in val.items():
-    #         if 'Ferrite' in subkey:
-    #             temp.append(np.mean(subvalue[:,0,0]))
-    #         else:
-    #             martensite_strain.append(np.mean(subvalue[:,0,0]))
-    #     ferrite_strain.append(np.mean(temp))
-
-    # fig, ax = plt.subplots(1, 1, figsize=(12,8))
-
-    # ax.plot(ferrite_strain, ferrite_stress, label='Avg. Stress Ferrite', linewidth=4)
-    # ax.plot(martensite_strain, martensite_stress, label='Avg. Stress Martensite', linewidth=4)
-    # ax.legend(fontsize=24)
-    # ax.set_xlabel('Strain', fontsize=22)
-    # ax.set_ylabel('Stress', fontsize=22)
-    # ax.tick_params(which='both', size=10, labelsize=20)
-
-    # fig.tight_layout()
-    # fig.savefig(path + r'/FlowcurvePartitioning.png')
-    # plt.close()
 
 
